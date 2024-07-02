600cbdd5cde15254ae9113220bd6974617a35fc0
YesungJang <c011335179@gmail.com>
2024-07-03 07:27:24 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// next.config.js
+module.exports = {
   output: "export",
+  basePath: "/your-repository-name", // your-repository-name을 GitHub 리포지토리 이름으로 변경
+  assetPrefix: "/your-repository-name/",
 };
-
-module.exports = nextConfig;
